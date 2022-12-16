61442e5ff173a5a07178a3fbb877f998c60727c7
Sharon Sawe <sawesharonn@gmail.com>
2022-12-16 09:52:10 +0300

    moreEdgeCases

--- src/test/java/org/junit/GraderTest.java
+++ src/test/java/org/junit/GraderTest.java
@@ -11,5 +11,27 @@ class GraderTest {
         var grader = new Grader();
         assertEquals('F', grader.determineGrade(59));
     }
+    @Test
+    void sixtyNineShouldReturnD(){
+        var grader = new Grader();
+        assertEquals('D', grader.determineGrade(69));
+    }
 
+    @Test
+    void seventyNineShouldReturnC(){
+        var grader = new Grader();
+        assertEquals('C', grader.determineGrade(79));
+    }
+
+    @Test
+    void eightNineShouldReturnB(){
+        var grader = new Grader();
+        assertEquals('B', grader.determineGrade(89));
+    }
+
+    @Test
+    void ninetyNineShouldReturnA(){
+        var grader = new Grader();
+        assertEquals('A', grader.determineGrade(99));
+    }
 }
