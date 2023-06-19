10e63738af98a3489272bcec155edbde8bd9a0bf
Sharon Sawe <sawesharonn@gmail.com>
2023-06-19 12:02:45 +0300

    grader

--- src/main/java/org/junit/Grader.java
+++ src/main/java/org/junit/Grader.java
@@ -2,22 +2,27 @@ package org.junit;
 
 public class Grader {
 
-    public char determineGrade(int numberGrade){
+    public static char determineGrade(int numberGrade){
         if (numberGrade < 0){
             throw new IllegalArgumentException("Invalid Grade");
         } else if (numberGrade < 60) {
+            System.out.println("F");
             return 'F';
         } else if (numberGrade < 70) {
+            System.out.println("D");
             return 'D';
         }else if (numberGrade < 80){
+            System.out.println("C");
             return 'C';
         }else if (numberGrade < 90){
+            System.out.println("B");
             return 'B';
         }else{
+            System.out.println("A");
             return 'A';
         }
     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        determineGrade(10);
     }
 }
--- src/main/java/org/junit/SimpleCalculator.java
+++ src/main/java/org/junit/SimpleCalculator.java
@@ -2,7 +2,16 @@ package org.junit;
 
 public class SimpleCalculator {
 
-    public int add(int numberA, int numberB){
-        return numberA + numberB;
+    public static int add(int numberA, int numberB){
+
+        int sum = numberA + numberB;
+
+        System.out.println("SUM : "+sum);
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        add(10,90);
     }
 }
